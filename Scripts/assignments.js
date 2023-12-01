aea0e9da3a2af1cd2749f504ddad0652c304eba0
Uditraj Singh Rathore <38015259+uditraj1@users.noreply.github.com>
2023-12-01 06:47:38 -0500

    Correct js logic

--- Scripts/assignments.js
+++ Scripts/assignments.js
@@ -4,21 +4,46 @@ class lectureList {
   showLectureList() {
     display(data);
     function display(data) {
-      document.querySelector(".section").innerHTML = "";
-      data.forEach((elem, index) => {
-        let div = document.createElement("div");
-        div.classList.add("lectures");
-        div.innerHTML = `
-          <div class="details">
-          <span>${elem.title}</span>
-          <button class="type">${elem.type}</button>
-          <p><b>${elem.instructor}</b> scheduled <b>Assignment problem's</b> at ${elem.schedule}
-            - ${elem.time}</p>
-        </div>
-        <div><button class="new">New</button></div>
+        document.querySelector(".section").innerHTML = "";
+
+        // Create a table element
+        let lectureTable = document.createElement('table');
+        lectureTable.classList.add('lectures-table');
+    
+        // Add table headings
+        let headingsRow = document.createElement('tr');
+        headingsRow.innerHTML = `
+            <th>Topic Name</th>
+            <th>Assignment Name</th>
+            <th>Assignment Description</th>
+            <th>File Attached</th>
+            <th>Action</th> <!-- New column for any action buttons -->
+            <th>Submission Action</th>
         `;
-        document.querySelector(".section").append(div);
-      });
+        lectureTable.appendChild(headingsRow);
+    
+        // Add rows to the table
+        data.forEach((elem) => {
+            let lectureRow = document.createElement('tr');
+            lectureRow.innerHTML = `
+                <td>${elem.topicName}</td>
+                <td>${elem.assignmentName}</td>
+                <td>${elem.assignmentDescription}</td>
+                <td>${elem.file}</td>
+                <td><button class="new" onclick="downloadFile('${elem.file}', '${elem.fileContent}')">Download</button></td>
+                <td><button class="new" onclick="submitAssignment('${elem.assignmentName}')">Submit Assignment</button></td>
+            `;
+            lectureTable.appendChild(lectureRow);
+        });
+    
+        // Append the table to the section
+        document.querySelector(".section").appendChild(lectureTable);
+
+        // Retrieve the selected course name from localStorage
+        let selectedCourseName = JSON.parse(localStorage.getItem('selectedCourseName')) || "YourSelectedCourseName";
+
+        // Update the displayed course name
+        document.getElementById("selectedCourseDisplay").innerHTML = `<span style="font-weight: bold;"> Selected Course: ${selectedCourseName}</span>`;
     }
   }
   filterByTiltle(value) {
@@ -102,4 +127,66 @@ function filterDate() {
 }
 function resetFilter() {
   window.location.reload();
+}
+function downloadFile(fileName, fileContent) {
+    // Create an anchor element
+    const anchor = document.createElement('a');
+
+    // Set the href attribute to a data URI containing the file content
+    anchor.href = `data:application/octet-stream;base64,${fileContent}`;
+    
+    // Set the download attribute to the file name
+    anchor.download = fileName;
+
+    // Simulate a click on the anchor element to start the download
+    anchor.click();
+}
+function displayAssignments(assignments) {
+    const assignmentSection = document.querySelector('.section');
+    assignmentSection.innerHTML = '';
+  
+    // Create a table element
+    let assignmentTable = document.createElement('table');
+    assignmentTable.classList.add('assignments-table');
+  
+    // Add table headings
+    let headingsRow = document.createElement('tr');
+    headingsRow.innerHTML = `
+        <th>Topic Name</th>
+        <th>Assignment Name</th>
+        <th>Assignment Description</th>
+        <th>File Attached</th>
+        <th>Action</th> <!-- New column for any action buttons -->
+    `;
+    assignmentTable.appendChild(headingsRow);
+  
+    // Add rows to the table
+    assignments.forEach((assignment) => {
+      let assignmentRow = document.createElement('tr');
+      assignmentRow.innerHTML = `
+          <td>${assignment.topicName}</td>
+          <td>${assignment.assignmentName}</td>
+          <td>${assignment.assignmentDescription}</td>
+          <td>${assignment.fileInput}</td>
+          <td><button class="new">Download</button></td>
+      `;
+      assignmentTable.appendChild(assignmentRow);
+    });
+  
+    // Append the table to the section
+    assignmentSection.appendChild(assignmentTable);
+}
+function submitAssignment(assignmentName) {
+    // Find the assignment in the data array
+    const selectedAssignment = data.find(assignment => assignment.assignmentName === assignmentName);
+
+    if (selectedAssignment) {
+      // Store the selected assignment in localStorage
+      localStorage.setItem('currentAssignment', JSON.stringify(selectedAssignment));
+  
+      // Redirect to the submit_assignment.html page or handle the submission logic here
+      window.location.href = 'submit_assignment.html';
+    } else {
+      console.error('Assignment not found:', assignmentName);
+    }
 }
